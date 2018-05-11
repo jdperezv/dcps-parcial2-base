4ed255b377cfa4cfb4cad505d962a81ebc35dc52
jdperezv <jdperezv@unal.edu.co>
2018-05-11 12:11:06 -0500

    Actualización

--- app/adapters/author.js
+++ app/adapters/author.js
@@ -4,7 +4,9 @@ export default ApplicationAdapter.extend({
     keyForAtrtribute(attr){
         switch(attr){
             case 'firstName':
-            return 'firstName'
+            return 'firstName';
+            default:
+            return this._super(...arguments);
 
         }
     }
